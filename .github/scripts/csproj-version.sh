3234cdbf0f662f84e485b887ea86d3cf8903a499
Vadim Vorotilin <vadim.vorotilin@gmail.com>
2021-09-28 18:14:12 +0300

    Regular expression fix

--- .github/scripts/csproj-version.sh
+++ .github/scripts/csproj-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cat $1 |\
-    grep -Eo '<Version>[0-9]+\.[0-9]+\.[0-9]+-.*<\/Version>' |\
+    grep -Eo '<Version>[0-9]+\.[0-9]+\.[0-9]+(?:-(?:[a-z]|[A-Z])+)?<\/Version>' |\
     perl -pe 's/<Version>//g and s/<\/Version>//g'
