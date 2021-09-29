29c47fadfd4d7eae2edde33d8cd640711508fea3
Vadim Vorotilin <vadim.vorotilin@gmail.com>
2021-09-29 18:51:01 +0300

    Tag-commit script fix

--- .github/scripts/tag-commit.sh
+++ .github/scripts/tag-commit.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-ver=`./csproj-version.sh $1`
+if [ -z ${1+x} ]; then
+    echo "The script expects 1 argument: the project file path."
+    exit 2
+fi
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ver=`bash ${__dir}/csproj-version.sh $1`
 
 echo "Project version is ${ver}. Tagging..."
 
